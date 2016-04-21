29cba5bccbfcf8e153abf6929a8ca4e8b6d1aec6
Sam <nt123p@yahoo.com.tw>
2016-04-21 20:56:14 +0800

    add radio button

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -2,25 +2,53 @@ package com.example.user.simpleui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView mTvHelloWorld;
-    EditText mEdtInput;
+    TextView mTextView;
+    EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTvHelloWorld = (TextView) findViewById(R.id.textView);
+        mTextView = (TextView) findViewById(R.id.textView);
+        mEditText = (EditText) findViewById(R.id.editText);
 
+        //註冊實體鍵盤事件
+        mEditText.setOnKeyListener(new View.OnKeyListener() { //參數為Interface
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+                    click(v);
+                    return true; //攔截，不繼續執行
+                }
+                return false;
+            }
+        });
+
+        //註冊虛擬鍵盤事件
+        mEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    click(v);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
-    public void click(View view){
-        mTvHelloWorld.setText("Android Class");
+    public void click(View view){ //view為觸發的元素
+        String text = mEditText.getText().toString();
+        mTextView.setText(text);
+        mEditText.setText("");
     }
 }
