a9b402c016d88deef3e210bae47e4fa4c9a6734f
Sam <nt123p@yahoo.com.tw>
2016-04-21 19:56:41 +0800

    add button and edittext

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.user.simpleui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTvHelloWorld;
+    EditText mEdtInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTvHelloWorld = (TextView) findViewById(R.id.textView);
+
+    }
+
+    public void click(View view){
+        mTvHelloWorld.setText("Android Class");
     }
 }
